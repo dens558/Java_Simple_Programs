8fcd779cfc29f72826abbdd6dfc3c5d5dcab8759
Linuxbook <mdm314@icloud.com>
2018-03-19 21:00:03 -0400

    Fixed Gibberish  -changed out from String to StringBuilder  -fixed for loop

--- src/Gibberish.java
+++ src/Gibberish.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
+import java.util.Random;
 
-public class Gibberish { //Generates a random string of characters //Note: untested
+public class Gibberish { //Generates a random string of characters
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         System.out.println("How many characters?");
         int num = in.nextInt();
         in.close();
-        String[] chars = "abcdefghijklmnopqrstuvwxyz".split("");
-        String out = "";
-        for (int i = 0; i>=num;i++) {
-            out += chars[(int) Math.round(Math.random()*chars.length)];
+        String chars = "abcdefghijklmnopqrstuvwxyz";
+        StringBuilder out = new StringBuilder();
+        Random rand = new Random();
+        for (int i = 0; i <= num; i++) {
+            int c = rand.nextInt(chars.length());
+            out.append(chars.charAt(c));
         }
         System.out.println(out);
     }
--- src/Prime.java
+++ src/Prime.java
@@ -4,10 +4,10 @@ public class Prime {  //Inputs a number and checks if it is prime
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         boolean run = true;
-        while(run) {
+        while (run) {
             System.out.println("Enter a number to see if it is prime");
             int num = in.nextInt();
-            if(isPrime(num)) {
+            if (isPrime(num)) {
                 System.out.println(num + " is prime.");
             } else {
                 System.out.println(num + " is not prime");
@@ -21,8 +21,8 @@ public class Prime {  //Inputs a number and checks if it is prime
     }
 
     private static boolean isPrime(int n) {
-        for (int i = n-1; i >1;i--) {
-            if (n%i == 0) {
+        for (int i = n - 1; i > 1; i--) {
+            if (n % i == 0) {
                 return false;
             }
         }
